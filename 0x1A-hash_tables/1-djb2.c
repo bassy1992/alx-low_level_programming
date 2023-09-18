1c051992e73548836b1dad9a11b219584dd2d390
bassy1992 <wyarquah@gmail.com>
2023-09-18 23:01:23 +0000

    update

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,9 @@
-#include "hash_tables.h"
-
 /**
- * hash_djb2 - function to compute hash of 'str' using djb2
- * @str: string to hash
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
- * Return: value of 64 bits hash
+ * Return: hash value
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -18,4 +15,4 @@ unsigned long int hash_djb2(const unsigned char *str)
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 	return (hash);
-
+}
