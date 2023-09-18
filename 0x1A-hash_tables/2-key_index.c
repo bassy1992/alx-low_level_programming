ec9f367eac2066d71f80932b0692e928c5a173a3
bassy1992 <wyarquah@gmail.com>
2023-09-18 22:44:14 +0000

    update

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,49 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index - get index for key given
- * @key: key (string) to hash
- * @size: size of hash table
+ * key_index - Get the index at which a key/value
+ *      pair should be stored in array of a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
  *
- * Return: index of 'key' using djb2
+ * Return: The index of the key.
+ * Description: Uses the djb2 algorithm.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
-
-
-________________________________
-
-
-2-main.c
-
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "hash_tables.h"
-
-/**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
-{
-    char *s;
-    unsigned long int hash_table_array_size;
-
-    hash_table_array_size = 1024;
-    s = "cisfun";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "Don't forget to tweet today";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-    s = "98";
-    printf("%lu\n", hash_djb2((unsigned char *)s));
-    printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));  
-    return (EXIT_SUCCESS);
-}
-
